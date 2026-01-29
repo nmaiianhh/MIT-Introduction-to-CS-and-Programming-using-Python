3dc40f14acb4bb9b35a21cbf70c8ad2b8282d4f1
nmaiianhh <nmaianh10@gmail.com>
2026-01-29 19:01:15 +0000

    add lecture 2 and 3

---
+++ lecture-3.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 17 22:58:53 2026
+
+@author: nmaia
+"""
+
+# You are in the Lost Forest.
+# *********************************
+#         X
+# *********************************
+
+# Go left or right
+
+# where = input('You are in the Lost Forrest. Go left or right? ')
+# while where == 'right':
+#     where = input('You are in the Lost Forrest. Go left or right? ')
+# print('You got out of the Lost Forrest.')
+
+
+# #################################################################################
+# n = int(input('Enter a non-negative interger: '))
+# while n>0:
+#     print('x')
+#     n -= 1
+  
+# while True:
+#     print('no')
+
+
+#################################################################################
+# n = 0
+# where = input('You are in the Lost Forrest. Go left or right? ')
+# while where == 'right':
+#     n += 1
+#     print(n)
+#     where = input('Go left or right? ')
+  
+# print('You got out of the Lost Forrest.')
+
+#################################################################################
+# x = 4
+# i = 1
+# factorial = 1 
+# while i <= x:
+#     factorial *= i
+#     i += 1
+# print (f'{x} factorial is {factorial}')
+
+#################################################################################
+
+# for n in range(4,0,-1):
+#     print ('$'*n)
+    
+#################################################################################
+
+# mysum = 0
+# for i in range(10):
+#     mysum += i
+# print(mysum)
+#this is to find the sum from 0 to 10. it's 45
+
+#################################################################################
+
+# mysum = 0
+# start = 3
+# end = 5
+# for i in range (start, end + 1):
+#     print('i =', i)
+#     mysum += i
+# print(mysum)
+
+#################################################################################
+
+#Finger Exercises Lecture 3
+N = int(input('Choose a number: '))
+for i in range(N):
+    print('hello world')
+
+
+
+
+
+
